d12f7c7d4c1a201c8c5caa2a3464b1dda0f44554
Christian Jarani <majula@majula.fios-router.home>
2017-11-17 01:13:29 -0500

    Finished(?) debugging simulation, working on PR alg's

--- vmsim.java
+++ vmsim.java
@@ -9,38 +9,60 @@ import java.util.Random;
 import java.lang.Math;
 
 public class vmsim {
-	
-	// System Fields (32-bit)
+
+	 ////////////////////////////
+	// System Fields (32-bit) //
+   ////////////////////////////	
+
 	private static final int PAGE_SIZE 	  = (int) Math.pow(2,12);		// 2^12 (4KB)
 	private static final int ADDRESS_SIZE = (int) Math.pow(2,32);		// 2^32 (all enumerable addresses for a 32-bit system)
-	private static final int NUM_PAGES 	  = ADDRESS_SIZE / PAGE_SIZE;	// 2^32 / 2^12 = 2^20 (total # of address divided by page size)
-	
-	private static void report(String algorithm, int numFrames, int numRefs, int numFaults, int numWrites){}
+	private static final int NUM_PAGES 	  = (int) Math.pow(2,20);		// 2^32 / 2^12 = 2^20 (total # of address divided by page size)
 
-	// Page Replacement Algorithms
-	private static void opt(int[] frameTable, RefEntry[] refTable, int numFrames, int numRefs) {
+
+	 /////////////////////////////////
+	// Page Replacement Algorithms //
+   /////////////////////////////////
+
+	// OPT will evict a page based on if/when it's referenced in the future
+	private static int opt(int[] frameTable, RefEntry[] refTable, int numFrames, int numRefs) {
 		int evictFrame = -1, evictFutureRef = -1;
 		for(int i = 0; i < numFrames; i++) {
 
 		}
+		return 0;
 	}
-	private static void clock(int[] frameTable, int numFrames) {}
+
+	// CLOCK will evict the oldest unreferenced page, using its position in a circular queue as a measure of history
+	private static int clock(int[] frameTable, int numFrames) {
+		return 0;
+	}
 
 	private static int random(int numFrames) {
 		Random rand = new Random();
 		return rand.nextInt(numFrames);			// Return a random frame between 1st and last addresses in frame table
 	}
 
-	private static void nru(int[] frameTable, int numFrames, int refresh) {}
+	// NRU will continuously check if a page has been referenced/modified during page management, even when evicition is unnecessary.
+	// 	 Similar to CLOCK, but age is not represented implicitly by the position in a queue.
+	//	 Rather, it is represented explicitly thru use of the dirty bit and referenced bit (which is reset after an amt of time called the refresh period)
+	private static int nru(int[] frameTable, int numFrames, int refresh) {
+		return 0;
+	}
 
-	private static void test(String algorithm, int numFrames, int refresh, File tracefile) {
+
+
+	 //////////////////////
+	// Simulation Logic //
+   //////////////////////
+
+	private static void simulate(String algorithm, int numFrames, int refresh, File tracefile) {
 
 		// Algorithm Choice (quicker than using .compareTo every interation of our simulation)
-		int algChoice;
-			 if(algorithm.compareTo("opt") 	  == 0) algChoice == 0;
-		else if(algorithm.compareTo("clock")  == 0) algChoice == 1;
-		else if(algorithm.compareTo("random") == 0) algChoice == 2;
-		else if(algorithm.compareTo("nru")	  == 0) algChoice == 3;
+		int algChoice = 4;
+			 if(algorithm.compareTo("opt") 	  == 0) algChoice = 0;
+		else if(algorithm.compareTo("clock")  == 0) algChoice = 1;
+		else if(algorithm.compareTo("random") == 0) algChoice = 2;
+		else if(algorithm.compareTo("nru")	  == 0) algChoice = 3;
 
 		// Algorithm Statistics
 		int usedFrames = 0, numRefs = 0, numFaults = 0, numWrites = 0; 
@@ -59,46 +81,45 @@ public class vmsim {
 		for(int i = 0; i < numFrames; i++) {	// Initialize all frames to 'empty' status
 			frameTable[i] = -1;
 		}
-		RefEntry[] refTable = new RefEntry[(int) Math.pow(2,20)];		// Create lookup table for memory references from file (offline setting)
-		
-		String[] 	tokens 	= null;										// Will hold the separated address & mode (read or write)
-		String 		line 	= reader.nextLine();						// Read first line of file
+
+		RefEntry[] refTable = new RefEntry[NUM_PAGES];		// Create lookup table for memory references from file (offline setting)
+		String[]   tokens 	= null;										// Will hold the separated address & mode (read or write)
+		String 	   line;						// Read first line of file
 		while(reader.hasNextLine()) {									// And keep reading until there is nothing left to read (EOF)
+			line = reader.nextLine();										// Read next line
 			tokens = line.split("\\s");										// Split line into address and mode
 			refTable[numRefs] = new RefEntry(tokens[0],tokens[1]);			// Make new RefEntry from contents of tokens
-			line = reader.nextLine();										// Read next line
 			numRefs++;														// Increment total number of memory references made
 		}
-
+		
 		for(int i = 0; i < numRefs; i++) {
-			int  pAddress = Integer.parseInt(refTable[i].getAddress()) >> 12; 	// Translate virtual address (Integer.parseInt(refTable[i].getAddress())) to page address ( >> 12)
-			char mode 	  = refTable[i].getMode();								// Find out if we were reading or writing to the address
+			long temp = Long.parseLong(refTable[i].getAddress(),32); 	// Get full virtual address
+			int  pAddress = (int) temp >>> 12;							// Logical shift 12 bits to isolate our page address
+			char mode = refTable[i].getMode();							// Find if we are reading or writing
 
 			//	   if(algChoice == 0)	set up shit OPT needs
  			//else if(algChoice == 3)	set up shit NRU needs
 
 			if(!pageTable[pAddress].isValid()) {		// If page address is invalid (page fault)
+
 				numFaults++;								// Increment running count of page faults
-				
 				if(usedFrames < numFrames) {				// Compulsary Miss: we still have open frames (no eviction necessary)
 					for(int j = 0; j < numFrames; j++) {		// Find an open frame
-						if(frameTable[i] == -1) {					// If this frame is open
-							frameTable[i] = pAddress;					// Set frame to hold requested page
+						if(frameTable[j] == -1) {					// If this frame is open
+							frameTable[j] = pAddress;					// Set frame to hold requested page
 							pageTable[pAddress].setFrameNum(i);			// Associate page with frame it's being loaded into
 							usedFrames++;								// Increment number of used frames in frame table
 						}
 					}
-				}
-				else {										// Capacity Miss: we need to evict something
-					int evictionIndex;						// How we select the page depends on what algorithm we chose
-						 if(algChoice == 0) evictionIndex = opt(frameTable,refTable,numFrames,numRefs);		// OPT will make the eviction based on if/when a page is referenced in the future
-					else if(algChoice == 1) evictionIndex = clock(frameTable,numFrames);					// CLOCK will evict the oldest unreferenced page, using its position in a circular queue as a measure of history
-					else if(algChoice == 2) evictionIndex = random(numFrames);								// RANDOM will choose a frame at random to evict
-					else if(algChoice == 3) evictionIndex = nru(frameTable,numFrames,refresh);				// NRU will continuously check if a page has been referenced/modified during page management, even when evicition is unnecessary.
-																											//		Similar to CLOCK, but age is not represented implicitly by the position in a queue.
-																											//		Rather, it is represented explicitly thru use of the dirty bit and referenced bit (which is reset after an amt of time called the refresh period)
+				} else {									// Capacity Miss: we need to evict something
+					
+					int evictionIndex = 0;
+						 if(algChoice == 0) evictionIndex = opt(frameTable,refTable,numFrames,numRefs);
+					else if(algChoice == 1) evictionIndex = clock(frameTable,numFrames);
+					else if(algChoice == 2) evictionIndex = random(numFrames);
+					else if(algChoice == 3) evictionIndex = nru(frameTable,numFrames,refresh);
+
 					int pageToEvict = frameTable[evictionIndex];	// Get address of page we want to evict
-					//frameTable[evictionIndex] = -1; usedFrames--;	// Evict page, signify corresponding frame is now unused
 					pageTable[pageToEvict].setFrameNum(-1);			// Disassociate frame from page
 					pageTable[pageToEvict].setValid(false);			// Set valid bit to 0 (signal page no longer is loaded into a frame)
 					pageTable[pageToEvict].setRef(false);			// Set referenced bit to 0 (signal page has not been referenced)
@@ -106,7 +127,6 @@ public class vmsim {
 						pageTable[pageToEvict].setDirty(false);			// Set dirty bit to 0 (signal page has not been modified)
 						numWrites++;									// Write page back to disk (increment running count of disk writes)
 					}
-					// Else we have a clean eviction (page not modified)
 					frameTable[evictionIndex] = pAddress;
 					pageTable[pAddress].setFrameNum(i);
 				}
@@ -115,8 +135,16 @@ public class vmsim {
 			pageTable[pAddress].setRef(true);				// Set referenced bit to 1 (signal page has been referenced)
 			if(mode == 'W')									// If page is written to
 				pageTable[pAddress].setDirty(true);				// Set dirty bit to 1 (signal page has been modified)
-		}
+		} 
 		report(algorithm,numFrames,numRefs,numFaults,numWrites);
+	}
+
+	private static void report(String algorithm, int numFrames, int numRefs, int numFaults, int numWrites){
+		System.out.println("Algorithm:\t" + algorithm);
+		System.out.println("Number of frames:\t" + numFrames);
+		System.out.println("Total memory accesses:\t" + numRefs);
+		System.out.println("Total page faults:\t" + numFaults);
+		System.out.println("Total writes to disk:\t" + numWrites);
 	}
 
 	public static void main(String args[]) {
@@ -141,7 +169,7 @@ public class vmsim {
 					f = new File(args[5]);
 					System.out.println("Reading from " + args[5] + "...");
 				}
-				test(algorithm,numFrames,refresh,f);
+				simulate(algorithm,numFrames,refresh,f);
 			}
 		}
 	}
